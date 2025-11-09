a9be39835515d639fd3711b1b63802a6ecdcb862
Michael-Batiuk <Michael.Batiuk@stu.ocsb.ca>
2025-11-09 16:43:06 -0500

    Turns on and off an LED

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael Batiuk
+ * Created on: Nov 2025
+ * This program turns on and off an LED
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// a button
+input.onButtonPressed(Button.A, function () {
+
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// b button
+input.onButtonPressed(Button.B, function () {
+
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
